97bc9e7024f00439bab2ee4305953f63e56aaed8
Elizabeth Munoz <munoz_714@live.com>
2020-05-21 18:38:01 -0700

    pulling timeblocks to deploy page

--- controllers/api/availabilityController.js
+++ controllers/api/availabilityController.js
@@ -4,51 +4,51 @@ const router = require("express").Router();
 /**
  * Post - Read All
  */
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
   db.Timeblocks.findAll(req.query)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
+    .then((dbModel) => res.json(dbModel))
+    .catch((err) => res.status(422).json(err));
 });
 
 /**
  * TImeblocks - Read One
  */
-router.get("/:id", function (req, res) {
+router.get("/:id", function(req, res) {
   db.Timeblocks.findById(req.params.id)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
+    .then((dbModel) => res.json(dbModel))
+    .catch((err) => res.status(422).json(err));
 });
 
 /**
  * Timeblocks - Create
  * Notice how we are also taking in the User Id! Important!
  */
-router.post("/", function (req, res) {
+router.post("/", function(req, res) {
   console.log(req.body);
   db.Timeblocks.create({
     UserId: req.user.id,
     start_time: req.body.start_time,
     end_time: req.body.end_time,
   })
-    .then(dbModel => res.json(dbModel))
+    .then((dbModel) => res.json(dbModel))
     .catch((err) => res.status(422).json(err));
 });
 
 /**
  * Timeblocks - Update
  */
-router.put("/:id", function (req, res) {
+router.put("/:id", function(req, res) {
   db.Timeblocks.update(req.body, { where: { id: req.params.id } })
-    .then(dbModel => res.json(dbModel))
+    .then((dbModel) => res.json(dbModel))
     .catch((err) => res.status(422).json(err));
 });
 
 /**
  * Timeblocks - Delete
  */
-router.delete("/:id", function (req, res) {
+router.delete("/:id", function(req, res) {
   db.Timeblocks.destroy({ where: { id: req.params.id } })
-    .then(dbModel => res.json(dbModel))
+    .then((dbModel) => res.json(dbModel))
     .catch((err) => res.status(422).json(err));
 });
 
--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -9,45 +9,50 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 /**
  * Home Page
  */
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
   res.render("index", { user: req.user });
 });
 
 /**
  * Home Page, again
  */
-router.get("/home", function (req, res) {
+router.get("/home", function(req, res) {
   res.render("index", { user: req.user });
 });
 
-// shows my Availability
-// router.get("/availability", isAuthenticated, function (req, res) {
-//   res.render("availability", { user: req.user });
-// });
-
-router.get("/availability", isAuthenticated, function (req, res) {
+router.get("/availability", isAuthenticated, function(req, res) {
   console.log("req.user: ", req.user);
   db.Timeblocks.findAll({
     where: { UserId: req.user.id },
     raw: true,
     // include: [db.User]
   }) // Joins User to Timeblock! And scrapes all the seqeulize stuff off
-    .then(dbModel => {
-      console.log(dbModel);
-      res.render("availability", { user: req.body, Timeblocks: dbModel });
+    .then((dbModel) => {
+      console.log(req.user);
+      res.render("availability", { user: req.user, Timeblocks: dbModel });
     })
-    .catch(err => res.status(422).json(err));
+    .catch((err) => res.status(422).json(err));
+});
+
+router.get("/deploy", isAuthenticated, function(req, res) {
+  console.log("req.user: ", req.user);
+  db.Timeblocks.findAll() // Joins User to Timeblock! And scrapes all the seqeulize stuff off
+    .then((dbModel) => {
+      console.log(dbModel);
+      res.render("deploy", { user: req.user, Timeblocks: dbModel });
+    })
+    .catch((err) => res.status(422).json(err));
 });
 
 // display User console page
-router.get("/profile", isAuthenticated, function (req, res) {
+router.get("/profile", isAuthenticated, function(req, res) {
   res.render("profile", { user: req.user });
 });
 
 /**
  * Signup page
  */
-router.get("/signup", function (req, res) {
+router.get("/signup", function(req, res) {
   if (req.user) {
     res.redirect("/");
   } else {
@@ -58,7 +63,7 @@ router.get("/signup", function (req, res) {
 /**
  * Login page
  */
-router.get("/login", function (req, res) {
+router.get("/login", function(req, res) {
   if (req.user) {
     res.redirect("/");
   } else {
@@ -70,18 +75,18 @@ router.get("/login", function (req, res) {
  * Forum Page -
  * Notice loading our posts, with that include!
  */
-router.get("/deploy", isAuthenticated, function (req, res) {
-  db.Post.findAll({ raw: true, include: [db.User] }) // Joins User to Posts! And scrapes all the seqeulize stuff off
-    .then(dbModel => {
-      res.render("deploy", { user: req.user, posts: dbModel });
-    })
-    .catch(err => res.status(422).json(err));
-});
+// router.get("/deploy", isAuthenticated, function(req, res) {
+//   db.Post.findAll({ raw: true, include: [db.User] }) // Joins User to Posts! And scrapes all the seqeulize stuff off
+//     .then((dbModel) => {
+//       res.render("deploy", { user: req.user, posts: dbModel });
+//     })
+//     .catch((err) => res.status(422).json(err));
+// });
 
 /**
  * Generic Error Page
  */
-router.get("*", function (req, res) {
+router.get("*", function(req, res) {
   res.render("errors/404", { user: req.user });
 });
 
